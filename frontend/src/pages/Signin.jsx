8e39e5786c4e2bb7be7c4f321a972b6d2fd0cc67
toripineda <victoriaip004@gmail.com>
2025-10-16 16:13:47 -0400

    signin/up styling and layout. no functionality

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,8 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import "./Signin.css"
+import "./Transcript";
+
 export default function Signin() {
+  const navigate = useNavigate();
+
+  const handleSignupClick = () => {
+    navigate("./Transcript");
+  }
+
   return (
-    <div>
-      <h1>Sign In!</h1>
-      <p>Welcome to the home page of our webapp!</p>
+    <div className="signin-page">
+      <div className="signin-card">
+        <h1>Sign In</h1>
+
+        <label>Email:</label>
+        <input type="email" placeholder="Enter your email" />
+
+        <label>Password:</label>
+        <input type="password" placeholder="Enter your password" />
+
+        <button onClick={handleSignupClick}>Sign in</button>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,8 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import "./Signup.css"
+import "./Transcript";
+
 export default function Signup() {
+  const navigate = useNavigate();
+
+  const handleSignupClick = () => {
+    navigate("./Transcript");
+  }
+
   return (
-    <div>
-      <h1>Sign Up</h1>
-      <p>This page tells you about our application.</p>
+    <div className="signup-page">
+      <div className="signup-card">
+        <h1>Sign Up</h1>
+
+        <label>First Name:</label>
+        <input type="text" placeholder="Enter your first name" />
+
+        <label>Last Name:</label>
+        <input type="text" placeholder="Enter your last name" />
+
+        <label>Email:</label>
+        <input type="email" placeholder="Enter your email" />
+
+        <label>Password:</label>
+        <input type="password" placeholder="Create a password" />
+
+        <button onClick={handleSignupClick}>Create Account</button>
+      </div>
     </div>
+
   );
 }
